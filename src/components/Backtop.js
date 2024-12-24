304477570615f9fe526dfee5643abd203828caab
Gagandeep <gagandeep_c@outlook.com>
2024-12-24 19:52:48 +0530

    BAcktop change

--- src/components/Backtop.js
+++ src/components/Backtop.js
@@ -3,6 +3,6 @@ import React from "react";
 export function Backtop(){
     
     return(
-        <a href="hello" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+        <a href="" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
     )
 }
